87d4e58e07a93473f23b51876ac3cc1d0a934051
holgerchristensen <holger.gott@icloud.com>
2023-03-17 09:39:10 +0100

    links and a few more tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ extern crate core;
 
 
 use crate::datatype::{Context, Problem, Solution};
-use crate::parse::{parse_constraint, parse_problem, parse_term};
 use crate::r#match::match_;
 use crate::simpl::simpl;
 use crate::substs::problem_substitution;
@@ -13,30 +12,6 @@ mod simpl;
 mod r#match;
 mod parse;
 mod print;
-
-
-#[test]
-fn calculator1() {
-    println!("{:#?}", parse_term("λx:*. λy:*. N").print());
-    println!("{:#?}", parse_term("λx:*. λy:*. N"));
-    println!("{:#?}", parse_term("N O P E").print());
-    println!("{:#?}", parse_term("N O P E"));
-    println!("{:#?}", parse_term("N O (P E)").print());
-    println!("{:#?}", parse_term("N O (P E)"));
-    println!("{:#?}", parse_term("N (O P E)").print());
-    println!("{:#?}", parse_term("N (O P E)"));
-    println!("{:#?}", parse_term("N (λx:*. n)").print());
-    println!("{:#?}", parse_term("N (λx:*. n)"));
-    println!("{:#?}", parse_term("(λx:*. λy:*. n h) N").print());
-    println!("{:#?}", parse_term("(λx:*. λy:*. n h) N"));
-    println!("{:#?}", parse_term("(λx:*. (λy:*. n) h) N").print());
-    println!("{:#?}", parse_term("(λx:*. (λy:*. n) h) N"));
-    println!("{:#?}", parse_constraint("I u32 =? option (option u32)").print());
-    println!("{:#?}", parse_constraint("I u32 =? option (option u32)"));
-    println!("{:#?}", parse_problem("I u32 =? option u32 ∧ I string =? option bool").print());
-    println!("{:#?}", parse_problem("I u32 =? option u32 ∧ I string =? option bool"));
-}
-
 
 fn main_huet(context: &mut Context, problem: Problem) {
     let p_simpl = simpl(context.clone(), problem);
@@ -77,30 +52,26 @@ mod tests {
     use std::cell::RefCell;
     use std::collections::HashMap;
     use std::rc::Rc;
-    use crate::datatype::{Context};
+    use crate::datatype::{Context, SolutionSet};
     use crate::main_huet;
-    use crate::parse::{parse_problem, parse_type};
+    use crate::parse::{parse_constraint, parse_problem, parse_term, parse_type};
 
-    macro_rules! test_harness {
-        ($l:literal) => {
-            {
-                // Arrange
-                let problem = parse_problem($l);
+    fn run(input: &str) -> SolutionSet {
+        // Arrange
+        let problem = parse_problem(input);
 
-                let mut context = generate_context();
+        let mut context = generate_context();
 
-                // Act
-                main_huet(&mut context, problem.clone());
+        // Act
+        main_huet(&mut context, problem.clone());
 
-                // Assert
-                println!("Problem: {}", problem);
-                println!();
-                println!("Number of solutions: {:#?}", context.solutions.borrow().len());
-                let minimal = context.minimal_solutions();
-                println!("Context: {}", minimal);
-                minimal
-            }
-        };
+        // Assert
+        println!("Problem: {}", problem);
+        println!();
+        println!("Number of solutions: {:#?}", context.solutions.borrow().len());
+        let minimal = context.minimal_solutions();
+        println!("Context: {}", minimal);
+        minimal
     }
 
     fn generate_context() -> Context {
@@ -118,79 +89,112 @@ mod tests {
                 ("I".to_string(), vec!["j".to_string()]),
                 ("L".to_string(), vec!["m".to_string()]),
                 ("P".to_string(), vec!["q".to_string(), "r".to_string()]),
+                ("T".to_string(), vec!["u".to_string(), "v".to_string()]),
             ]),
         }
     }
 
     #[test]
     fn example_1() {
-        test_harness!("I u32 =? option u32");
+        run("I u32 =? option u32");
     }
 
     #[test]
     fn example_2() {
-        test_harness!("I u32 =? option (option u32)");
+        run("I u32 =? option (option u32)");
     }
 
     #[test]
     fn example_3() {
-        test_harness!("I (I u32) =? option (option u32)");
+        run("I (I u32) =? option (option u32)");
     }
 
     #[test]
     fn example_4() {
-        test_harness!("I (L u32) =? option (option u32)");
+        run("I (L u32) =? option (option u32)");
     }
 
     #[test]
     fn example_5() {
-        test_harness!("I u32 =? option u32 ∧ I string =? option string");
+        run("I u32 =? option u32 ∧ I string =? option string");
     }
 
     #[test]
     fn example_6() {
-        test_harness!("I u32 =? option u32 ∧ I string =? option u32");
+        run("I u32 =? option u32 ∧ I string =? option u32");
     }
 
     #[test]
     /// This case should fail as there should not be any solutions.
     fn example_7() {
-        let solutions = test_harness!("I u32 =? option u32 ∧ I string =? option bool");
+        let solutions = run("I u32 =? option u32 ∧ I string =? option bool");
         assert!(solutions.0.is_empty());
     }
 
     #[test]
     fn example_8() {
-        test_harness!("I u32 =? result u32 string");
+        run("I u32 =? result u32 string");
     }
 
     #[test]
     fn example_9() {
-        test_harness!("I (I u32) =? option u32");
+        run("I (I u32) =? option u32");
     }
 
     #[test]
     fn example_10() {
-        test_harness!("P u32 u32 =? result u32 u32");
+        run("P u32 u32 =? result u32 u32");
     }
 
     #[test]
     fn example_11() {
-        test_harness!("I u32 =? result u32 u32");
+        run("I u32 =? result u32 u32");
     }
 
     #[test]
     fn example_12() {
-        test_harness!("I u32 =? result string string");
+        run("I u32 =? result string string");
     }
 
     #[test]
     fn example_13() {
-        test_harness!("I u32 =? result u32 string");
+        run("I u32 =? result u32 string");
     }
 
     #[test]
     fn example_14() {
-        test_harness!("I (L u32) =? result u32 string");
+        run("I (L u32) =? result u32 string");
+    }
+
+    #[test]
+    fn example_15() {
+        run("I (L u32) =? option (option u32) ∧ L (I u32) =? option (option u32)");
+    }
+
+    #[test]
+    fn example_16() {
+        run("P u32 u32 =? result u32 u32 ∧ T u32 u32 =? result u32 u32 ");
+    }
+
+    #[test]
+    fn parse_and_print() {
+        println!("{}   ", parse_term("λx:*. λy:*. N"));
+        println!("{:#?}", parse_term("λx:*. λy:*. N"));
+        println!("{}   ", parse_term("N O P E"));
+        println!("{:#?}", parse_term("N O P E"));
+        println!("{}   ", parse_term("N O (P E)"));
+        println!("{:#?}", parse_term("N O (P E)"));
+        println!("{}   ", parse_term("N (O P E)"));
+        println!("{:#?}", parse_term("N (O P E)"));
+        println!("{}   ", parse_term("N (λx:*. n)"));
+        println!("{:#?}", parse_term("N (λx:*. n)"));
+        println!("{}   ", parse_term("(λx:*. λy:*. n h) N"));
+        println!("{:#?}", parse_term("(λx:*. λy:*. n h) N"));
+        println!("{}   ", parse_term("(λx:*. (λy:*. n) h) N"));
+        println!("{:#?}", parse_term("(λx:*. (λy:*. n) h) N"));
+        println!("{}   ", parse_constraint("I u32 =? option (option u32)"));
+        println!("{:#?}", parse_constraint("I u32 =? option (option u32)"));
+        println!("{}   ", parse_problem("I u32 =? option u32 ∧ I string =? option bool"));
+        println!("{:#?}", parse_problem("I u32 =? option u32 ∧ I string =? option bool"));
     }
 }
